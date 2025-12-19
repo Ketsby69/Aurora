357567d661b29ccde7e61fd38e1ef996bc46d41c
Kevin Xavier Fregoso Romero <kevinxavierfregosoromero@gmail.com>
2025-12-18 23:49:10 -0600

    add class encoder text characteristics

--- configurations/config_path.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-PATH_MEMORY = BASE_DIR / "memory" / "database_encoder_characteristics.pkl"
--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-import os
-import pickle
-
-from configurations import PATH_MEMORY
+from sensory_neuron import EncoderTextCharacteristics
 
 
 if __name__ == '__main__':
-
-    if not os.path.exists(PATH_MEMORY):
-
-        with open(PATH_MEMORY, "wb") as f:
-
-            pickle.dump({}, f, protocol=pickle.HIGHEST_PROTOCOL)
-    
-    from sensory_neuron import EncoderTextCharacteristics
 
     interaction = input("interaction: ")
 
